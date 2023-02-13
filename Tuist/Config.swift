1014d8281d0891ba3edd8a465b14c98e90406954
sasha <sashatv126@gmail.com>
2023-02-13 17:16:22 +0300

    add config

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -1,11 +1,5 @@
 import ProjectDescription
 
 let config = Config(
-    compatibleXcodeVersions: ["12.0"],
-    swiftVersion: "5.4.0",
-    generationOptions: .options(
-        xcodeProjectName: "SomePrefix-\(.projectName)-SomeSuffix",
-        organizationName: "Sasha",
-        developmentRegion: "ru"
-    )
+    generationOptions: []
 )
